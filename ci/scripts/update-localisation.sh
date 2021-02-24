2a024693635c90a9b036864b84f9bb818efb390d
Ruben Gonzalez Criado <ruben.criado@yoti.com>
2021-02-24 14:11:25 +0000

    YM-21838: Order the steps to have two different commits for each file

--- ci/scripts/update-localisation.sh
+++ ci/scripts/update-localisation.sh
@@ -29,9 +29,9 @@ git config user.email "ci@yoti.com"
 git config user.name "yoti-ci"
 
 git add $YOTI_ANDROID_IN_PATH
-git add $POSTOFFICE_ANDROID_IN_PATH
-
 git diff-index --quiet HEAD $YOTI_ANDROID_OUT_PATH || git commit -m "Update Yoti strings from spreadsheet merge"
+
+git add $POSTOFFICE_ANDROID_IN_PATH
 git diff-index --quiet HEAD $POSTOFFICE_ANDROID_OUT_PATH || git commit -m "Update PostOffice strings from spreadsheet merge"
 
 ## iOS
@@ -40,7 +40,7 @@ git config user.email "ci@yoti.com"
 git config user.name "yoti-ci"
 
 git add $YOTI_IOS_FILE_NAME
-git add $POSTOFFICE_IOS_FILE_NAME
-
 git diff-index --quiet HEAD $YOTI_IOS_FILE_NAME || git commit -m "Update Yoti strings from spreadsheet merge"
+
+git add $POSTOFFICE_IOS_FILE_NAME
 git diff-index --quiet HEAD $POSTOFFICE_IOS_FILE_NAME || git commit -m "Update PostOffice strings from spreadsheet merge"
